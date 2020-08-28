2a51abf3bc441f6873d03f9a2124ae218a489217
Miller Rich <william.miller.rich@gmail.com>
2020-08-28 14:26:52 -0500

    add variables to script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var queryURL =  "api.nytimes.com/svc/search/v2/articlesearch.json?q= + query + &api-key=yourkey"
-var query = document.getElementById() 
+var query = document.getElementById("#query");
+var records = document.getElementById("#records");
+var startYear = document.getElementById("#start-year");
+var endYear = document.getElementById("#end-year"); 
 
 $.ajax({
     url: queryURL,
